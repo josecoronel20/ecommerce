371ef1d6e946084a8d59a01ccc60928adc8b19b7
josecoronel20 <josecoronel20@outlook.com>
2024-09-12 20:26:51 +0100

    creando aplicacion

---
+++ src/app/products/offer/page.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function offer () {
+    return (<p>page offer</p>)
+}
---
+++ src/app/products/page.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function products () {
+    return (<p>page productos</p>)
+}
