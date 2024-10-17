5409f0c33513f030bf2df77a2abf89b1dad4cd04
josecoronel20 <josecoronel20@outlook.com>
2024-10-17 07:23:44 +0100

    primera instancia de componenten initial-searchBar

--- src/app/components/header/cartComponents/cartCard/CartCard.tsx
+++ src/app/components/header/cartComponents/cartCard/CartCard.tsx
@@ -2,40 +2,38 @@ import React from "react";
 import Quantity from "../quantity/Quantity";
 import Delete from "../delete/Delete";
 import PriceIndividual from "../priceIndividual/PriceIndividual";
-import { CartItems, HandlerDeleteParams, HandlerQuantityParams } from "../../../../utils/types";
+import {
+  CartItems,
+  HandlerDeleteParams,
+  HandlerQuantityParams,
+} from "../../../../utils/types";
 
-
-export default function CartCard({item, handlerQuantity, handlerDelete}: {
-    item: CartItems;
-    handlerQuantity: HandlerQuantityParams;
-    handlerDelete: HandlerDeleteParams;
-  }){
-
-
-    //todo:corregir bg size de cards
-    //cambiar img por image de next
-return (<><div className="grid grid-cols-2 gap-1 col-span-2">
-    <img
-      className="bg-colorLight2 rounded"
-      src={item.images[0]}
-      alt={`img de ${item.title}`}
-    />
-    <h3 className="text-xs">{item.title}</h3>
-  </div>
-  <div className="flex flex-col gap-1 justify-start items-center">
-    <Quantity
-      product={item}
-      handlerQuantity={handlerQuantity}
-    />
-    <Delete
-      productId={item.id}
-      handlerDelete={handlerDelete}
-    />
-  </div>
-  <PriceIndividual
-    quantity={item.quantity}
-    price={item.price}
-  /></>
-
-)
+export default function CartCard({
+  item,
+  handlerQuantity,
+  handlerDelete,
+}: {
+  item: CartItems;
+  handlerQuantity: HandlerQuantityParams;
+  handlerDelete: HandlerDeleteParams;
+}) {
+  //todo:corregir bg size de cards
+  //cambiar img por image de next
+  return (
+    <>
+      <div className="grid grid-cols-2 gap-1 col-span-2">
+        <img
+          className="bg-colorLight2 rounded"
+          src={item.images[0]}
+          alt={`img de ${item.title}`}
+        />
+        <h3 className="text-xs">{item.title}</h3>
+      </div>
+      <div className="flex flex-col gap-1 justify-start items-center">
+        <Quantity product={item} handlerQuantity={handlerQuantity} />
+        <Delete productId={item.id} handlerDelete={handlerDelete} />
+      </div>
+      <PriceIndividual quantity={item.quantity} price={item.price} />
+    </>
+  );
 }
--- src/app/components/header/searchbar/SearchBar.tsx
+++ src/app/components/header/searchbar/SearchBar.tsx
@@ -1,32 +1,48 @@
 "use client";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { useRef } from "react";
 import { useState } from "react";
 
 export default function SearchBar() {
   const [isFocus, setIsFocus] = useState(false);
+  const router = useRouter();
+
+  const [searchText, setSearchText] = useState<string>("");
 
   const inputRef = useRef<HTMLInputElement>(null);
 
   const focusInput = () => {
-      inputRef.current?.focus();
+    inputRef.current?.focus();
   };
+
+  const handlerSearchText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  };
+
+  const handlerOnSubmit = (e:React.FormEvent) => {
+    e.preventDefault()
+router.push(`products/search=${searchText}`);
+  }
+
 
   return (
     <div data-testid="search-bar" className="relative">
+      <form onSubmit={handlerOnSubmit}>
       <input
         onFocus={() => setIsFocus(true)}
         onBlur={() => setIsFocus(false)}
         ref={inputRef}
         className={` w-full rounded`}
-      />
+        onChange={handlerSearchText}
+      /></form>
       {!isFocus && (
         <button
           onClick={() => focusInput()}
           className={` absolute right-0 top-0 flex  text-colorLight3`}
         >
-          <MagnifyingGlassIcon className="size-6 text-colorDark1"/>
+          <MagnifyingGlassIcon className="size-6 text-colorDark1" />
         </button>
       )}
     </div>
